847bb4ea339818f9f323ee872cd2d0333ceb407b
chengzhe-xu <xuchengzhe2018@gmail.com>
2023-10-09 23:04:27 -0700

    [2023/10/09] switch to __half or fp16 mod for SIMT preparation

--- HelloWorld/cuda/src/hello_world_cuda.cu
+++ HelloWorld/cuda/src/hello_world_cuda.cu
@@ -6,6 +6,14 @@ __global__ void hello_world_cuda() {
 }
 
 int main() {
+    int deviceId;
+    cudaGetDevice(&deviceId);
+    cudaDeviceProp props;
+    cudaGetDeviceProperties(&props, deviceId);
+    int computeCapabilityMajor = props.major;
+    int computeCapabilityMinor = props.minor;
+    int multiProcessorCount = props.multiProcessorCount;
+    printf("Device ID: %d\nNumber of SMs: %d\nCompute Capability %d.%d\n", deviceId, multiProcessorCount, computeCapabilityMajor, computeCapabilityMinor);
     hello_world_cuda<<<16, 32>>>();
     cudaDeviceSynchronize();
     return 0;
