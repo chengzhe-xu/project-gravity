7112479a1943f59649b9890ba6a1135c345b9b55
chengzhe-xu <xuchengzhe2018@gmail.com>
2023-10-12 10:03:45 -0700

    [2023/10/12] add simt test cases

--- MatMul/test_matrix_mul.py
+++ MatMul/test_matrix_mul.py
@@ -29,9 +29,16 @@ if __name__=='__main__':
         matC = np.ascontiguousarray(matC.astype(np.float32))
 
         matC_ref = np.matmul(matA.copy(), matB.copy()) + matC.copy()
+
         matC_naive_exp = matrix_mul_lib.mat_mul_naive(matA.T, matB, matC.copy(), M, N, K)
         naive_abs_err, naive_abs_rel, naive_rel_err = abs_error_w_rel(matC_naive_exp.copy(), matC_ref.copy())
+
         matC_half_exp = matrix_mul_lib.mat_mul_half(matA.T.astype(np.float16), matB.astype(np.float16), matC.copy().astype(np.float16), M, N, K)
         half_abs_err, half_abs_rel, half_rel_err = abs_error_w_rel(matC_half_exp.copy(), matC_ref.astype(np.float16).copy())
+
+        matC_simt_exp = matrix_mul_lib.mat_mul_simt(matA.astype(np.float16), matB.T.astype(np.float16), matC.copy().astype(np.float16), M, N, K)
+        simt_abs_err, simt_abs_rel, smit_rel_err = abs_error_w_rel(matC_simt_exp.copy(), matC_ref.astype(np.float16).copy())
+
         print(f"Maximum abs error: naive version: abs: {naive_abs_err} ({100*naive_abs_rel}%)\
-              half version: abs: {half_abs_err} ({100*half_abs_rel}%)")
+              half version: abs: {half_abs_err} ({100*half_abs_rel}%)\
+              SIMT version abs: {simt_abs_err} ({100*simt_abs_rel}%)")
